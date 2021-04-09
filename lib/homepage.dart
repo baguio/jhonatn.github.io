5c6189e6b8c4dbfa531ad3fa0713c5256b9b3e0e
Jhonatan A <1548170+baguio@users.noreply.github.com>
2021-04-08 20:57:08 -0500

    HomePage: Moved to its own file
    Also, renamed project from "fluttertest" to "site"

---
+++ lib/homepage.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:site/sided_row.dart';
+import 'package:site/window_sized_box.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      backgroundColor: CupertinoColors.white,
+      child: SingleChildScrollView(
+        child: StartBox(),
+      ),
+    );
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const TextStyle titleFont = TextStyle(
+  fontSize: 78,
+  color: Color.fromRGBO(29, 29, 31, 1),
+  fontWeight: FontWeight.w600,
+  letterSpacing: -0.002,
+  height: 1.05,
+);
+
+const TextStyle messageFont = TextStyle(
+  fontSize: 21,
+  color: Color.fromRGBO(134, 134, 139, 1),
+  fontWeight: FontWeight.w400,
+  letterSpacing: -0.022,
+);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class StartBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WindowSizedBox(
+      child: Column(
+        children: [
+          SidedRow(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(40, 70, 0, 40),
+              child: AutoSizeText.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(text: "Jhonatan Avalos", style: titleFont),
+                    TextSpan(text: "\nSoftware developer. Mainly mobile stuff", style: messageFont),
+                  ],
+                ),
+                minFontSize: 10,
+              ),
+            ),
+          ),
+          SidedRow(
+            alignment: Alignment.bottomRight,
+            child: OverflowBox(
+              alignment: Alignment.topLeft,
+              maxHeight: double.infinity,
+              child: Image(
+                image: AssetImage(
+                  'assets/iphone11_cam.jpg'
+                ),
+              ),
+            )
+          ),
+        ],
+      ),
+    );
+  }
+}
