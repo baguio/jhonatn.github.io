5fefe86966c7168589eb3490c694e47bdbfa44cc
Jhonatan A <1548170+baguio@users.noreply.github.com>
2021-04-08 20:57:24 -0500

    Site title updated

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
+      title: "Jhonatan A.",
       home: HomePage(),
     );
   }
