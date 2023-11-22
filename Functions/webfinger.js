5ff5591ae949b71de9c6e39165a50d146b1326c9
Jhonatan A <1548170+jhonatn@users.noreply.github.com>
2023-11-21 21:51:45 -0500

    Code style

--- Functions/webfinger.js
+++ Functions/webfinger.js
@@ -2,19 +2,19 @@ const originAccountDomain = "jhona.tn";
 
 const redirections = {
   // Implement your redirections mapping here
-    "dev" : {
-        user: "jhonatn",
-        domain: "mastodon.social"
-    },
-    "me" : {
-        user: "jhonatn",
-        domain: "mastodon.social"
-    }
+  "dev": {
+    user: "jhonatn",
+    domain: "mastodon.social"
+  },
+  "me": {
+    user: "jhonatn",
+    domain: "mastodon.social"
+  }
 };
 
 exports.handler = async (event) => {
   var accountHandle = "";
-    var accountUri = "";
+  var accountUri = "";
   try {
     const { resource } = event.queryStringParameters || {};
 
@@ -23,50 +23,50 @@ exports.handler = async (event) => {
       originAccountDomain
     );
     accountHandle = _accountHandle;
-      accountUri = resource;
+    accountUri = resource;
   } catch (error) {
     return {
       statusCode: 400
     };
   }
 
-    if (!redirections[accountHandle]) {
-      return {
-          statusCode: 404
-      };
-    }
-
-    const redirection = redirections[accountHandle];
-    const newURL = generateWebFingerRequestURL(
-      redirection.domain,
-      redirection.user,
-      accountUri
-    );
-
+  if (!redirections[accountHandle]) {
     return {
-      statusCode: 302,
-      headers: {
-        Location: newURL,
-      },
+      statusCode: 404
     };
+  }
+
+  const redirection = redirections[accountHandle];
+  const newURL = generateWebFingerRequestURL(
+    redirection.domain,
+    redirection.user,
+    accountUri
+  );
+
+  return {
+    statusCode: 302,
+    headers: {
+      Location: newURL,
+    },
+  };
 };
 
 function extractAccountHandle(acct, originAccountDomain) {
-    let accountHandle = acct.replace(/^acct:/, '');
+  let accountHandle = acct.replace(/^acct:/, '');
 
-    const domainIndex = accountHandle.indexOf('@' + originAccountDomain);
-    if (domainIndex !== -1) {
-        return accountHandle.substring(0, domainIndex);
-    }
+  const domainIndex = accountHandle.indexOf('@' + originAccountDomain);
+  if (domainIndex !== -1) {
+    return accountHandle.substring(0, domainIndex);
+  }
 
-    return accountHandle;
+  return accountHandle;
 }
 
 function generateWebFingerRequestURL(newHost, newUser, originalResource) {
-    var newUrl = new URL("https://" + originAccountDomain);
-    newUrl.host = newHost;
-    newUrl.pathname = '/.well-known/webfinger';
-    newUrl.searchParams.set("resource", `acct:${newUser}@${newHost}`);
+  var newUrl = new URL("https://" + originAccountDomain);
+  newUrl.host = newHost;
+  newUrl.pathname = '/.well-known/webfinger';
+  newUrl.searchParams.set("resource", `acct:${newUser}@${newHost}`);
 
-    return newUrl.toString();
+  return newUrl.toString();
 }
